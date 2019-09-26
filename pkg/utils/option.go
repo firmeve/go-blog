ab72994b94a27ecfa87d04b6a9a50bc7d63efe33
simon <simon@crcms.cn>
2019-09-26 09:59:14 +0800

    Add options

--- pkg/utils/option.go
+++ pkg/utils/option.go
@@ -1,7 +1,13 @@
 package utils
 
+type Option interface{}
 
+type OptionFunc func(option Option)
 
-func ApplyOption()  {
-	
+func ApplyOption(option Option, options ...OptionFunc) Option {
+	for _, opt := range options {
+		opt(option)
+	}
+
+	return option
 }
