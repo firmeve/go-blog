2d5dd8feae53e861f6a7ccc4d518d4114a87465f
crcms <crcms@crcms.cn>
2019-09-29 23:48:37 +0800

    page transfrom testing

--- internal/document/transfromers/page.go
+++ internal/document/transfromers/page.go
@@ -1,6 +1,7 @@
 package transfromers
 
 import (
+	"fmt"
 	"github.com/blog/internal/document/models"
 	"github.com/blog/internal/pkg/transform"
 )
@@ -9,8 +10,19 @@ type Page struct {
 	*transform.BaseTransform
 }
 
+func (p *Page) GetData1Field() interface{} {
+	fmt.Println("GGGGGGGGGGGGGGGGG")
+	return p.Resource().(*models.Page).Data1.Src
+}
+
+func (t *Page) GetTitleField() string {
+	return t.Resource().(*models.Page).Title
+}
+
 func NewPage(page *models.Page) *Page {
-	return &Page{
+	transform := &Page{
 		BaseTransform:transform.NewTransform(page),
 	}
+	transform.BaseTransform.SetSource(transform)
+	return transform
 }
